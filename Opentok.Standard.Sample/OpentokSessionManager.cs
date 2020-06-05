18e4771fee39c0363f15d43d8da86154d6fd02bd
AresDev <armando.restrepo@gmail.com>
2020-06-04 21:19:04 -0500

    Update Opentok to Opentok.Standard update sample projects to .Net Framework 4.7.2

---
+++ Opentok.Standard.Sample/OpentokSessionManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opentok.Standard.Sample
+{
+    public class OpentokSessionManager
+    {
+    //    <add key = "OTKey" value="46440112" />
+    //<add key = "OTSecret" value="433e0d1e0fc6d247ac3ee6c5bbb9021212a8fb57" />
+        private readonly string key = "46440112";
+        private readonly string secret = "433e0d1e0fc6d247ac3ee6c5bbb9021212a8fb57";
+
+        public string Token { get; set; }
+        public string SessionId { get; set; }
+        public OpentokSessionManager()
+        {
+            //this.key = key;
+            //this.secret = secret;
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+        }
+
+        public async Task GetTokenAsync()
+        {
+            await Task.Run(() =>
+            {
+                int apiKey = Convert.ToInt32(this.key);
+                OpenTokSDK.OpenTok opentok = new OpenTokSDK.OpenTok(apiKey, this.secret);
+                OpenTokSDK.Session opentoksession = opentok.CreateSession(mediaMode: OpenTokSDK.MediaMode.ROUTED, archiveMode: OpenTokSDK.ArchiveMode.MANUAL);
+                Token = opentoksession.GenerateToken(role: OpenTokSDK.Role.PUBLISHER);
+                SessionId = opentoksession.Id;
+            });
+        }
+
+    }
+}
---
+++ Opentok.Standard.Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Opentok.Standard.Sample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            OpentokSessionManager sm = new OpentokSessionManager();
+            await sm.GetTokenAsync().ConfigureAwait(false);
+            Console.WriteLine(sm.SessionId);
+            Console.WriteLine(sm.Token);
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
